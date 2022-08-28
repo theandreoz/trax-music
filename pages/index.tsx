9a2a484865caed168cdb41e3effaee517cdc3e81
Andrés Olivé <andresolive@gmail.com>
2022-08-28 20:53:45 +0200

    feat: sign in and sign up logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div>Home</div>;
+};
 
 export default Home;
