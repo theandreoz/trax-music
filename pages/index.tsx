b80ea4cef21a3948c62611c296cb6f6058afd0bb
Andrés Olivé <andresolive@gmail.com>
2022-09-01 13:57:08 +0200

    style: title & subtitle UI layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,14 @@ import GradientLayout from '../components/gradientLayout';
 
 const Home = () => {
   return (
-    <GradientLayout color='red'>
+    <GradientLayout
+      roundImage='true'
+      color='red'
+      subtitle='profile'
+      title='Andres'
+      description='15 public playlists'
+      image='https://www.mavantidecorations.nl/media/catalog/product/cache/df536ffd4d95d6b4916466950bd4deb8/B/S/BS006_thumb.jpg'
+    >
       <div>HOMEPAGE!</div>
     </GradientLayout>
   );
