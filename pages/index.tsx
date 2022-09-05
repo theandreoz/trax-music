76ca6f76e1a0832fb781a470da0a9c55797305f3
Andrés Olivé <andresolive@gmail.com>
2022-09-05 11:31:26 +0200

    feat: artists list UI layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { Box, Text, Flex } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/react';
 import GradientLayout from '../components/gradientLayout';
 import prisma from '../lib/prisma';
 
-const Home = (/* { artists } */) => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       roundImage='true'
@@ -11,7 +13,31 @@ const Home = (/* { artists } */) => {
       description='15 public playlists'
       image='https://www.mavantidecorations.nl/media/catalog/product/cache/df536ffd4d95d6b4916466950bd4deb8/B/S/BS006_thumb.jpg'
     >
-      <div>HOMEPAGE!</div>
+      <Box color='white' paddingX='40px'>
+        <Box marginBottom='40px'>
+          <Text fontSize='2xl' fontWeight='bold'>
+            Top artist this month
+          </Text>
+          <Text fontSize='md'>Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX='10px' width='20%'>
+              <Box bg='gray.900' borderRadius='4px' padding='15px' width='100%'>
+                <Image
+                  src='https://placebeard.it/360x360/any'
+                  alt='avatar'
+                  borderRadius='100%'
+                />
+                <Box marginTop='20px'>
+                  <Text fontSize='large'>{artist.name}</Text>
+                  <Text fontSize='x-small'>Artist </Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
 };
